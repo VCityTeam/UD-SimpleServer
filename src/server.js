490d190485aea0c04fd88e3a4bbab98b54201275
CorentinLIRIS <corentin.chignin@hotmail.fr>
2021-12-02 16:45:58 +0100

    Other try on cors

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 /** @format */
 const express = require('express');
-const cors = require('./cors');
+const cors = require('cors');
 
 const SimpleServerModule = class SimpleServer {
   constructor() {}
 
   start(config) {
     const app = express();
-    //serve
+    //servedfsd
+
     app.use(express.static(config.folder)); //what folder is served
     
-    app.use(cors);
+    app.use(cors())
+
+    app.get('/',function(req,res,next){
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
     //http server
     app.listen(config.port, function (err) {
       if (err) console.log('Error in server setup');
